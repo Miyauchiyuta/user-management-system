dbf31eaf03d541f71d197f4aae589c21d44667c3
Miyauchiyuta <yuta.miyauchi@fox-hound.jp>
2025-03-11 16:28:07 +0900

    day2,1-1完了しました。

---
+++ components/UserList.tsx
@@ -0,0 +1,18 @@
+import { Box, Typography } from '@mui/material';
+import { User } from '../types/User';
+import UserCard from '../components/UserCard';
+import React from 'react';
+
+interface UserListProps{
+    users:User[];
+}
+
+const UserList: React.FC<UserListProps> = ({users}) => {
+    return(
+        <>
+        {users.map(user => (
+            <UserCard key={user.id} user={user} />
+        ))}
+        </>
+    )
+}
