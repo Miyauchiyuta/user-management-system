b6e34e943a68927ea5d3f06884f5b89be41d65f6
Miyauchiyuta <yuta.miyauchi@fox-hound.jp>
2025-03-12 12:24:04 +0900

    day2,3-1完了しました。

---
+++ components/UserDetails.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { User } from '../types/User';
+import { Box, Card, CardContent, Typography } from '@mui/material';
+
+interface USerDetailsProps{
+    user: User;
+}
+
+const UserDetails:React.FC<USerDetailsProps> = ({ user }) => {
+    return(
+        <Card sx={{ maxWidth: 600, mx: 'auto', mt: 4 }}>
+          <Typography variant="h5" gutterBottom>
+            ユーザー詳細
+          </Typography>
+          <Box sx={{ mt: 2 }}>
+            <Typography>
+              ID: {user.id}
+            </Typography>
+            <Typography>
+              名前: {user.name}
+            </Typography>
+            <Typography>
+              メールアドレス: {user.email}
+            </Typography>
+            <Typography>
+              役割: {user.role}
+            </Typography>
+          </Box>
+      </Card>
+    )
+}
+
+export default UserDetails;
