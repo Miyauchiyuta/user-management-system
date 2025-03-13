c5a930da94c88b6e1e24120494f8c8cbcb340cb2
Miyauchiyuta <yuta.miyauchi@fox-hound.jp>
2025-03-13 11:09:44 +0900

    day3,task2-1完了しました

---
+++ components/part/CustomCard.tsx
@@ -0,0 +1,32 @@
+// components/parts/CustomCard.tsx
+
+import React from "react";
+import { Card, CardContent, Typography, CardActions } from "@mui/material";
+
+interface CustomCardProps {
+  title: string;
+  description: string;
+  actions?: React.ReactNode;
+}
+
+const CustomCard: React.FC<CustomCardProps> = ({
+  title,
+  description,
+  actions,
+}) => {
+  return (
+    <Card sx={{ minWidth: 275, mb: 2 }}>
+      <CardContent>
+      <Typography variant="h5" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="h5">
+          {description}
+        </Typography>
+      </CardContent>
+      {actions && <CardActions>{actions}</CardActions>}
+    </Card>
+  );
+};
+
+export default CustomCard;
